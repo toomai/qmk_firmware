1721677895107dd51737545714441297d98681c5
Loïc Stevens <loic.stevens@ingenico.com>
2021-03-18 11:49:29 +0100

    fix: move media left

--- keyboards/preonic/keymaps/toomai/keymap.c
+++ keyboards/preonic/keymaps/toomai/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_FN] = LAYOUT_preonic_grid( \
   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,      KC_F10,   KC_F11,   KC_F12,   \
-  _______,  KC_BRIU,  KC_P7,    KC_P8,    KC_P9,    XXXXXXX,  KC_PSCR,  XXXXXXX,  KC_MPLY,    KC_MPRV,  KC_MNXT,  KC_VOLU,  \
+  _______,  KC_BRIU,  KC_P7,    KC_P8,    KC_P9,    XXXXXXX,  KC_PSCR,  KC_MPRV,  KC_MPLY,    KC_MNXT,  XXXXXXX,  KC_VOLU,  \
   _______,  KC_BRID,  KC_P4,    KC_P5,    KC_P6,    XXXXXXX,  KC_CAPS,  KC_LEFT,  KC_DOWN,    KC_UP,    KC_RIGHT, KC_VOLD,  \
   _______,  KC_PDOT,  KC_P1,    KC_P2,    KC_P3,    KC_P0,    XXXXXXX,  XXXXXXX,  KC_PGDOWN,  KC_PGUP,  XXXXXXX,  KC_MUTE,  \
   _______,  _______,  _______,  _______,  KC_LEAD,  _______,  _______,  _______,  _______,    XXXXXXX,  XXXXXXX,  KC_ESC    \
